badbea4fe3616c0e0f48c60115560e459fcf9899
falimo <falimo@yahoo.com>
2017-11-15 13:37:01 +0000

    all styling and strikethrough - final

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,42 +3,39 @@ $(function() {
 		// task and produces an HTML representation using <li> tags
 		function taskHtml(task) {
 			var checkedStatus = task.done ? "checked" : "";
-			var liElement = '<li><div class="view"><input class=toggle type="checkbox"' + "data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div></li>';
+			var liClass = task.done ? "completed" : "";
+			var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' + '<div class="view"><input class=toggle type="checkbox"' + "data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div></li>';
 
 			return liElement;
 		}
 		// toggleTask takes in an HMTL representation of an
 		// event that fires from an HTML representation of
 		// the toggle checkbox and performs and API request 
-		// to gottle the value of the 'done' field
+		// to toggle the value of the 'done' field
 		function toggleTask(e) {
 			var itemId = $(e.target).data("id");
-			
-
 			var doneValue = Boolean($(e.target).is(':checked'));
-			
-
 			$.post("/tasks/" + itemId, {
 				_method: "PUT",
 				task: {
 					done: doneValue
 				}
+			}).success(function(data) {
+				var liHtml = taskHtml(data);
+				var $li = $("#listItem-" + data.id);
+				$li.replaceWith(liHtml);
+				$('.toggle').change(toggleTask);
 			});
-
 		}
 
 		$.get("/tasks").success( function( data ){
 			var htmlString = "";
-
 			$.each(data, function(index, task) {
-				
 				htmlString += taskHtml(task);
 			});
 			var ulTodos = $('.todo-list');
 			ulTodos.html(htmlString);
-
 			$('.toggle').change(toggleTask);
-
 		});
 
 		$('#new-form').submit(function(event) {
